d54e18318a680a24bb165193c3e6bcd8d5792f64
Alex Kravchenko <alexkravchenko1605@gmail.com>
2024-02-08 22:01:02 +0300

    feat:add layout and sider

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,34 +1,82 @@
-import React, { useState } from 'react';
-
-import reactLogo from '/react.svg';
-import viteLogo from '/vite.svg';
-import tsLogo from '/ts.svg';
+import 'antd/dist/antd.css';
 import './main-page.css';
 
+import React, { useState } from 'react';
+import { Layout, Menu } from 'antd';
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    TrophyFilled,
+    CalendarOutlined,
+    HeartFilled,
+    IdcardOutlined,
+} from '@ant-design/icons';
+
+const { Header, Sider, Content } = Layout;
+
 export const MainPage: React.FC = () => {
-    const [count, setCount] = useState(0);
+    const [collapsed, setCollapsed] = useState(false);
 
     return (
         <>
-            <div>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img src={reactLogo} className='logo react' alt='React logo' />
-                </a>
-                <a href='https://www.typescriptlang.org/' target='_blank'>
-                    <img src={tsLogo} className='logo' alt='TS logo' />
-                </a>
-            </div>
-            <h1>Vite + React + TS</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/pages/main-page.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+            <Layout>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={collapsed}
+                    style={{ backgroundColor: 'grey' }}
+                >
+                    <div className='logo'>
+                        <img src='/images/logo.png' alt='logo' />
+                    </div>
+                    <Menu
+                        mode='inline'
+                        items={[
+                            {
+                                key: '1',
+                                icon: <CalendarOutlined />,
+                                label: 'Календарь',
+                            },
+                            {
+                                key: '2',
+                                icon: <HeartFilled />,
+                                label: 'Тренировки',
+                            },
+                            {
+                                key: '3',
+                                icon: <TrophyFilled />,
+                                label: 'Достижения',
+                            },
+                            {
+                                key: '4',
+                                icon: <IdcardOutlined />,
+                                label: 'Профиль',
+                            },
+                            {
+                                key: '5',
+                                icon: <img src='/exit.svg' />,
+                                label: 'Выход',
+                            },
+                        ]}
+                    />
+                </Sider>
+                <Layout className='site-layout'>
+                    <Header className='site-layout-background' style={{ padding: 0 }}>
+                        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                            className: 'trigger',
+                            onClick: () => setCollapsed(!collapsed),
+                        })}
+                    </Header>
+                    <Content
+                        className='site-layout-background'
+                        style={{
+                            minHeight: 'calc(100vh - 64px)',
+                        }}
+                    >
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
         </>
     );
 };
